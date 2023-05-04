13f019131f9d9b40f94be35687143e887dbb5b68
mouatassim boulahsen <boulahsen.mouatassim@gmail.com>
2023-05-03 22:31:17 -0400

    ✨ feat(initiate): added initiate page

--- src/pages/initiate.tsx
+++ src/pages/initiate.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
+import styles from "@/styles/initiate.module.css";
 
 export default function Initiate() {
   return (
@@ -7,16 +8,21 @@ export default function Initiate() {
       <Head>
         <title>QuickCV | Initiate</title>
       </Head>
-      <main>
-        <h1>Initiate page</h1>
-        <ol>
-          <li>
-            <Link href="create/personalInfo">Personal Info</Link>
-          </li>
-          <li>
-            <Link href="create/summary">Summary</Link>
-          </li>
-        </ol>
+      <main className={styles.initiateWrapper}>
+        <h2 className={styles.initateFunFactTitle}>
+          First Impressions Matter!
+        </h2>
+        <p className={styles.initateFunFact}>
+          Recruiters typically spend only 6 seconds scanning a resume before
+          deciding if a candidate is worth considering. A well-structured,
+          appealing resume can make you stand out from the crowd in those
+          crucial few seconds.
+        </p>
+        <div className={styles.initateActions}>
+          <Link href="/create/personalInfo">
+            take the first step towards your dream job today!
+          </Link>
+        </div>
       </main>
     </>
   );
